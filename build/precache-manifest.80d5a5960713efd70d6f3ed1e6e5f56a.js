983e23a2b96119a7793e1b4a2824f892a2b0f782
kaairasif <asicse22@gmail.com>
2018-10-14 23:58:15 -0700

    init

---
+++ build/precache-manifest.80d5a5960713efd70d6f3ed1e6e5f56a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a4fd641f63ce6a37230",
+    "url": "/static/js/main.8a4fd641.chunk.js"
+  },
+  {
+    "revision": "ac47dc3490332da54b24",
+    "url": "/static/js/1.ac47dc34.chunk.js"
+  },
+  {
+    "revision": "8a4fd641f63ce6a37230",
+    "url": "/static/css/main.074c772a.chunk.css"
+  },
+  {
+    "revision": "4d6ee855d1c81cfd075a543a4d2474ff",
+    "url": "/index.html"
+  }
+];
